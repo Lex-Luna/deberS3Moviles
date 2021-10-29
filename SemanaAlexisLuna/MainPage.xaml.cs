b78ae205b3c9e6008f082eee5a57aff2bcf457c0
Lex <alexisgabriel.lu@gmail.com>
2021-10-29 05:23:25 -0500

    tarea creada

---
+++ SemanaAlexisLuna/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SemanaAlexisLuna
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Siguiente_Clicked(object sender, EventArgs e)
+        {
+            string usuario = txtUsuario.Text;
+            string contrasenia = txtContrasenia.Text;
+            await Navigation.PushAsync(new Page1(usuario,contrasenia));
+        }
+    }
+}
